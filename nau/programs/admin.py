7d01b952043a1c6c3729726b10a794e45e5193f7
asakeev01 <aidar.asakeev@gmail.com>
2024-10-21 15:15:58 -0500

    Added to admin panel

--- nau/programs/admin.py
+++ nau/programs/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+
+admin.site.register(Program)
+admin.site.register(HeaderImage)
+admin.site.register(Slider)
+admin.site.register(Link)
+admin.site.register(Block)
--- nau/programs/models.py
+++ nau/programs/models.py
@@ -4,6 +4,10 @@ from django.db import models
 class Program(models.Model):
     name = models.CharField(max_length=200)
 
+    def __str__(self):
+        return self.name
+
+
 class HeaderImage(models.Model):
     image = models.ImageField()
 
@@ -12,16 +16,25 @@ class Slider(models.Model):
     program = models.OneToOneField(Program, on_delete=models.CASCADE)
     images = models.ManyToManyField(HeaderImage)
 
+    def __str__(self):
+        return f"{self.program.name}'s slider"
+
 
 class Link(models.Model):
     program = models.ForeignKey(Program, on_delete=models.CASCADE)
     link = models.URLField(max_length=200)
+
+    def __str__(self):
+        return f"{self.program.name}'s link"
 
 
 class Block(models.Model):
     program = models.ForeignKey(Program, on_delete=models.CASCADE)
     text = models.TextField()
     image = models.ImageField()
+
+    def __str__(self):
+        return f"{self.program.name}'s block"
 
 
 
