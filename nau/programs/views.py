756ca29b8d3d6f5edc2ac5fc92c721c84fbbef46
asakeev01 <aidar.asakeev@gmail.com>
2024-10-24 14:06:25 -0500

    Added templates

--- nau/programs/views.py
+++ nau/programs/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 
+def detail(request, program_id):
+    return HttpResponse("You're looking at program %s." % program_id)
+
