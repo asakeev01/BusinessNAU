94024ab773db600e067ab059ce23d9a8e8821b19
asakeev01 <aidar.asakeev@gmail.com>
2024-12-05 15:18:32 -0600

    Added styles to main page

--- nau/main/views.py
+++ nau/main/views.py
@@ -2,11 +2,14 @@ from django.shortcuts import render
 
 from .models import MainPage
 from programs.models import Program
+from news.models import News
 
 def main_page(request):
     main_page_content = MainPage.get_solo()
     programs = Program.objects.all()
+    upcoming_news = News.objects.filter(type='Upcoming').order_by('-id')[:3]
     return render(request, 'main/main_page.html', {
         'main_page': main_page_content,
         'programs': programs,
+        'upcoming_news': upcoming_news,
     })
--- nau/news/models.py
+++ nau/news/models.py
@@ -7,13 +7,17 @@ class News(models.Model):
         ('Latest', 'Latest'),
         ('Upcoming', 'Upcoming')
     ]
+    image = models.ImageField(blank=True, null=True)
     type = models.CharField(max_length=10, choices=NEWS_CHOICES)
     name = models.CharField(max_length=200, blank=True, null=True)
     title = models.CharField(max_length=200, blank=True, null=True)
     header_image = models.ImageField(blank=True, null=True)
     link = models.URLField(blank=True, null=True)
-    content = RichTextField()
+    content = RichTextField(blank=True, null=True)
 
     def __str__(self):
         return self.name
+
+    class Meta:
+        verbose_name = "News"
 
