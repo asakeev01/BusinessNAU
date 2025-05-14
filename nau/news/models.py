ace8a37849e42ef5178b67ab824d24fa1642979b
asakeev01 <aidar.asakeev@gmail.com>
2025-05-13 23:00:32 -0500

    Added news details

--- nau/news/models.py
+++ nau/news/models.py
@@ -21,8 +21,6 @@ class News(models.Model):
     image = models.ImageField(blank=True, null=True)
     type = models.CharField(max_length=10, choices=NEWS_CHOICES)
     name = models.CharField(max_length=200, blank=True, null=True)
-    title = models.CharField(max_length=200, blank=True, null=True)
-    header_image = models.ImageField(blank=True, null=True)
     link = models.URLField(blank=True, null=True)
     content = RichTextField(blank=True, null=True)
 
--- nau/news/views.py
+++ nau/news/views.py
@@ -1,19 +1,24 @@
 from django.shortcuts import render
 from django.shortcuts import render, get_object_or_404
 
-from .models import News
+from .models import *
 
 
 def index(request):
     latest_news = News.objects.filter(type='Latest').order_by('-id')
     upcoming_news = News.objects.filter(type='Upcoming').order_by('-id')
+    config = NewsPageConfig.get_solo()
     context = {
         'latest_news': latest_news,
-        'upcoming_news': upcoming_news
+        'upcoming_news': upcoming_news,
+        'config': config
     }
     return render(request, 'news/news_list.html', context)
 
 
 def detail(request, pk):
     news = get_object_or_404(News, pk=pk)
-    return render(request, 'main/news_detail.html', news)
+    context = {
+        'news': news
+    }
+    return render(request, 'news/news_detail.html', context)
